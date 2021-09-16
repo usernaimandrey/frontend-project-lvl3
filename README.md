838eaab3b8d866d219eeda1757a949f0b2b2ad38
usernaimandrey <anshlyapnikov@yandex.ru>
2021-09-16 04:26:58 +0300

    update index.html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/usernaimandrey/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/usernaimandrey/frontend-project-lvl3/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/0b7a69bb3a642ff51511/maintainability)](https://codeclimate.com/github/usernaimandrey/frontend-project-lvl3/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+module.exports = {
+  mode: NODE_ENV,
+  entry: './src/index.js',
+   output: {
+     filename: 'main.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
