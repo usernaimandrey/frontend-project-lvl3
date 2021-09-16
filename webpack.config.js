94230da18f197ed8d0bb54ae0831be655c741dc9
usernaimandrey <anshlyapnikov@yandex.ru>
2021-09-16 23:42:27 +0300

    fix style.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
+const NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = {
   mode: NODE_ENV,
   entry: './src/index.js',
-   output: {
-     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
