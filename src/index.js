747ef97e0768086b8e90bd224428ab6a93d4a39a
usernaimandrey <anshlyapnikov@yandex.ru>
2021-10-09 22:52:19 +0300

    fix import botstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
 import app from './app.js';
 
 app();
--- src/render/renderModal.js
+++ src/render/renderModal.js
@@ -4,16 +4,6 @@ const renderModal = (modal, { postPreView }) => {
   const modalBody = modal.querySelector('.modal-body');
   const button = document.querySelector('.full-article');
 
-  // const body = document.querySelector('body');
-  // body.classList.add('modal-open');
-  // body.setAttribute('style', 'overflow: hidden; padding-right: 15px;');
-
-  // modal.classList.add('show');
-  // modal.setAttribute('style', 'display: block;');
-  // modal.setAttribute('aria-modal', true);
-  // modal.setAttribute('role', 'dialog');
-  // modal.removeAttribute('aria-hidden');
-
   title.textContent = postTitle;
   modalBody.textContent = postDescription;
   button.setAttribute('href', postLink);
